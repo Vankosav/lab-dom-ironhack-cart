764863e12c9d88b729bb6e7323397782522c56d1
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-03-02 18:01:40 +0100

    am trying

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  /*const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input').innertext;*/
+  
+  const price = product.querySelector('.price span').textContent; // get the price value
+  const quantity = product.querySelector('.quantity input').value; // get the quantity value
+  const subtotal = price * quantity; // calculate the subtotal
 
+  // Update the DOM element for the subtotal value
+  const subtotalElement = product.querySelector('.subtotal span');
+  subtotalElement.textContent = subtotal;
+  return subtotal;
   //... your code goes here
 }
+console.log('Calculating subtotal, yey!');
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  const products = document.getElementsByClassName('product');
+  console.log(products)
+  const newArray = [...products];
+  let endTotal = 0;
+  for (element of newArray) {
+    endTotal = endTotal + updateSubtotal(element);
+  }
+  document.querySelector('#total-value span').textContent = endTotal
+}
+
+
+  
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
+ 
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
-}
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget.getElementsByClassName('.action button');
+  
   console.log('The target in remove is:', target);
   //... your code goes here
+  const parent = document.getElementsByClassName('action');
+  const child = document.getElementsByClassName('btn btn-remove');
+  //let rem = parent.removeChild(child);
+  console.log(rem);
+  
+ parent.forEach(child => {
+    //button.addEventListener('click', () => removeProduct());
+    let rem = parent.removeChild(child);
+    console.log(rem)
+  });
+ 
+  
 }
 
 // ITERATION 5
