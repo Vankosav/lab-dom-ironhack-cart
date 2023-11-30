262388612983c2b367440d39f34ad2a5fc83db1d
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-11-30 13:48:03 +0100

    bonus iteration has some issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  /*const price = product.querySelector('.price span');
-  const quantity = product.querySelector('.quantity input').innertext;*/
+  
   
   const price = product.querySelector('.price span').textContent; // get the price value
   const quantity = product.querySelector('.quantity input').value; // get the quantity value
@@ -12,7 +11,6 @@ function updateSubtotal(product) {
   const subtotalElement = product.querySelector('.subtotal span');
   subtotalElement.textContent = subtotal;
   return subtotal;
-  //... your code goes here
 }
 console.log('Calculating subtotal, yey!');
 
@@ -37,33 +35,71 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget.getElementsByClassName('.action button');
-  
-  console.log('The target in remove is:', target);
-  //... your code goes here
-  const parent = document.getElementsByClassName('action');
-  const child = document.getElementsByClassName('btn btn-remove');
-  //let rem = parent.removeChild(child);
-  console.log(rem);
-  
- parent.forEach(child => {
-    //button.addEventListener('click', () => removeProduct());
-    let rem = parent.removeChild(child);
-    console.log(rem)
-  });
- 
-  
+  const target = event.currentTarget;
+  const row = target.parentNode.parentNode;
+  row.remove();
 }
-
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
+  
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+//... your code goes here
+  //const child = document.getElementsByClassName('.action button');
+  const removeButtons = document.querySelectorAll(".btn-remove");
+  console.log(removeButtons);
+  removeButtons.forEach((button) => {
+    button.addEventListener('click', removeProduct);
+  });
+  const addButton = document.getElementById("create");
+  console.log(addButton);
+  addButton.addEventListener('click', createProduct);
+})
   //... your code goes here
-});
+
+
+// ITERATION 5
+function createProduct() {
+  // Selecting input fields for product name and unit price
+  const productNameInput = document.querySelector('.create-product input[type="text"]');
+  const productPriceInput = document.querySelector('.create-product input[type="number"]');
+
+  // Extracting values from input fields
+  const productName = productNameInput.value;
+  const productPrice = parseFloat(productPriceInput.value); // Converting to a floating-point number
+
+  // Checking if both name and price are provided
+  if (productName.trim() === '' || isNaN(productPrice) || productPrice < 0) {
+    alert('Please enter a valid product name and price.');
+    return;
+  }
+
+  // Clearing input fields after extracting values
+  productNameInput.value = '';
+  productPriceInput.value = '0';
+
+  // Creating a new row for the product in the table
+  const tableBody = document.querySelector('tbody');
+  const newRow = tableBody.insertRow();
+
+  // Adding cells for name, unit price, quantity, subtotal, and remove button
+  const nameCell = newRow.insertCell(0);
+  const priceCell = newRow.insertCell(1);
+  const quantityCell = newRow.insertCell(2);
+  const subtotalCell = newRow.insertCell(3);
+  const removeCell = newRow.insertCell(4);
+
+  // Setting content and classes for the cells
+  nameCell.textContent = productName;
+  priceCell.textContent = `$${productPrice.toFixed(2)}`;
+  quantityCell.innerHTML = '<input type="number" min="0" value="1">';
+  subtotalCell.textContent = `$${productPrice.toFixed(2)}`;
+  removeCell.innerHTML = '<button class="btn btn-remove">Remove</button>';
+
+   // Adding event listener to the remove button
+   const removeButton = removeCell.querySelector('.btn-remove');
+   removeButton.addEventListener('click', removeProduct);
+   
+  // Recalculate totals when a new product is added
+  calculateAll();
+}
